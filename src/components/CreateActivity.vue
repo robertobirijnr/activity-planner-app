f2ce799e8a670acca646da84134f287757d6e452
robertobirijnr <robertobirijnr@hotmail.com>
2020-10-27 08:12:03 +0000

    createActivity component

---
+++ src/components/CreateActivity.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+ <button v-if="!isDisplayForm" class="button is-primary is-block is-alt is-large" @click="ShowHideForm" >New Activity</button>
+    <div v-if="isDisplayForm" class="create-form">
+        <h2>Create Activity</h2>
+    <form action="">
+        <div class="field">
+        <div class="label">Title</div>
+        <div class="control">
+            <input type="text" v-model="Newactivity.title" class="input" placeholder="Read a Bok">
+        </div>
+        </div>
+        <div class="field">
+        <div class="label">Title</div>
+        <div class="control">
+            <textarea  class="textarea"  v-model="Newactivity.note" placeholder="add note"></textarea>
+        </div>
+        </div>
+        <div class="field">
+        <div class="label">Title</div>
+        <div class="control">
+            <select v-model="Newactivity.category" class="select">
+            <option disabled value="">Please select One</option>
+            <option v-for="category in categories" :key="category.index">{{category.text}}</option>
+            </select>
+        </div>
+        </div>
+        <div class="field is-group">
+        <div class="control">
+            <button @click="createActivity" :disabled="!isFormValid" class="button is-link">Create</button>
+            <button @click="ShowHideForm" class="button is-link">Cancel</button>
+        </div>
+        
+        </div>
+    </form>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            categories:{
+                type:Object,
+                required:true
+            }
+            },
+        data() {
+            return {
+        isDisplayForm:false,
+          Newactivity:{
+            title:"",
+            note:"",
+            category:""
+          }
+            }
+        },
+    methods: {
+      ShowHideForm(){
+        this.isDisplayForm = !this.isDisplayForm
+      },
+      createActivity(){
+        alert(this.Newactivity)
+      }
+    },
+    computed:{
+    isFormValid(){
+      return this.Newactivity.title && this.Newactivity.note
+    },
+    }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,40 +22,7 @@
           <section class="container">
             <div class="columns">
               <div class="column is-3">
-                <button v-if="!isDisplayForm" class="button is-primary is-block is-alt is-large" @click="ShowHideForm" >New Activity</button>
-                <div v-if="isDisplayForm" class="create-form">
-                  <h2>Create Activity</h2>
-                <form action="">
-                  <div class="field">
-                    <div class="label">Title</div>
-                    <div class="control">
-                      <input type="text" v-model="Newactivity.title" class="input" placeholder="Read a Bok">
-                    </div>
-                  </div>
-                  <div class="field">
-                    <div class="label">Title</div>
-                    <div class="control">
-                      <textarea  class="textarea"  v-model="Newactivity.note" placeholder="add note"></textarea>
-                    </div>
-                  </div>
-                  <div class="field">
-                    <div class="label">Title</div>
-                    <div class="control">
-                     <select v-model="Newactivity.category" class="select">
-                       <option disabled value="">Please select One</option>
-                       <option v-for="category in categories" :key="category.index">{{category.text}}</option>
-                     </select>
-                    </div>
-                  </div>
-                  <div class="field is-group">
-                    <div class="control">
-                      <button @click="createActivity" :disabled="!isFormValid" class="button is-link">Create</button>
-                      <button @click="ShowHideForm" class="button is-link">Cancel</button>
-                    </div>
-                   
-                  </div>
-                </form>
-                </div>
+               <CreateActivity :categories="categories"/>
               </div>
               <div class="column is-9">
                 <div class="box content">
@@ -71,10 +38,13 @@
 
 <script>
 import ActivityItem from '@/components/ActivityItem'
+import CreateActivity from '@/components/CreateActivity'
+
 import {fetchActivities, fetchUsers,fetchCategories} from '@/api'
 export default {
   components:{
-    ActivityItem
+    ActivityItem,
+    CreateActivity
   },
   data() {
       return {
@@ -83,18 +53,10 @@ export default {
           user: { },
           activities: {},
           categories: {},
-          isDisplayForm:false,
-          Newactivity:{
-            title:"",
-            note:"",
-            category:""
-          }
+         
       }
   },
   computed:{
-    isFormValid(){
-      return this.Newactivity.title && this.Newactivity.note
-    },
     fullAppName(){
       return this.appName  +  ' by ' + this.creator
     },
@@ -118,14 +80,7 @@ export default {
    this.categories = fetchCategories()
   },
   
-   methods: {
-      ShowHideForm(){
-        this.isDisplayForm = !this.isDisplayForm
-      },
-      createActivity(){
-        alert(this.Newactivity)
-      }
-    },
+   
 }
 </script>
 
